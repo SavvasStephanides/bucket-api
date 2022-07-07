d4b6363e5d9274f7d82d9676378f231c1982b785
Savvas Stephanides <savvascyp@hotmail.com>
2022-07-07 11:12:55 +0100

    Commit message typo fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,5 +3,5 @@ git config --global user.email "savvascyp@hotmail.com"
 git config --global user.name "SavvasStephanides"
 git status
 git add -A
-git commit -m "Authomated deploy: $(date '+%d-%m-%Y %H:%M:%S')"
+git commit -m "🤖 [Automated deploy via Bucket API] $(date '+%d-%m-%Y %H:%M:%S')"
 git push --all -f https://${GITHUB_TOKEN}@github.com/SavvasStephanides/bucket.git
